430e046aa71b98872a245f4b935f22c1260f482f
Kizitora3000 <kutaku2000@gmail.com>
2024-01-17 11:08:43 +0900

    pp.122-128

--- object/object.go
+++ object/object.go
@@ -1,8 +1,37 @@
 package object
 
+import "fmt"
+
 type ObjectType string
+
+const (
+	NULL_OBJ    = "NULL"
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+)
 
 type Object interface {
 	Type() ObjectType
 	Inspect() string
 }
+
+// 構文解析(2章)によって，ソースコードの整数リテラルは ast.IntegerLiteral に変換する
+// 評価(3章)によって，ast.IntegerLiteral を評価する際に Object.Integer に変換する
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+
+type Null struct{}
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string  { return "null" }
